2a7799da093b4b6cd0611fe79bf5d1f1cf5e70c1
Jhon <jhoncavalli@hotmail.com>
2025-11-15 23:40:33 -0300

    adiciona estilos e fontes ao componente Intro e atualiza o index.html

--- iPhone/src/Components/Intro.jsx
+++ iPhone/src/Components/Intro.jsx
@@ -1,7 +1,7 @@
 function Intro() {
 
 
-    // eslint-disable-next-line no-unused-vars
+
     const specs = [
         { value: '6.3', label: 'Display Super Retina XDR', color: 'text-blue-600' },
         { value: 'A18 Pro', label: 'Chip mais Rápida', color: 'text-orange-500' },
@@ -11,17 +11,30 @@ function Intro() {
 
     return (
 
-        <section>
-            <div>
-                <h1>iPhone 17 Pro</h1>
-                <p>Titânio. Tão Forte. Tão leve. Tão Pro. </p>
-                <p>O design mais refinado que já criamos. Titânio de grau aeroespascial. Chip A18 Pro.</p>
+        <section className="bg-black py-20 px-6">
+            <div className="max-w-5xl mx-auto text-center">
+                <h1 className="text-6xl md:text-8xl font-bold mb-6">iPhone 17 Pro</h1>
+                <p className="text-2xl md:text-4xl mb-4 text-gradient font-bold">Titânio. Tão Forte. Tão leve. Tão Pro. </p>
+                <p className="text-lg md:text-xl text-gray-300 mb-10 max-w-3xl mx-auto">O design mais refinado que já criamos. Titânio de grau aeroespascial. Chip A18 Pro.</p>
             </div>
 
-            <div>
-                <button>Compre agora</button>
-                <button>Saiba mais</button>
+            <div className="flex flex-col md:flex-row gap-4 justify-center items-center mb-16">
+                <button className= "bg-blue-500 hover:bg-blue-600 px-8 py-4 rounded-full font-medium transition-all duration-300 text-white shadow-lg shadow-blue-500/50 cursor-pointer">Compre agora</button>
+                <button className=" border-2 border-white rounded-full hover:bg-white hover:text-black texte-white px-8 py-4 text-white font-medium transition-all duration-300 cursor-pointer">Saiba mais</button>
             </div>
+
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mx-auto max-w-5xl">
+                {specs.map((spec, index) => (
+                    <div key={index} className="bg-gray-900 rounded-2xl p-6 hover:bg-gray-800 transition-all duration-300 hover:scale-105 cursor-pointer ">
+                        <p className= {`text-3xl font-bold mb-2 ${spec.color}`}>{spec.value}</p>
+                        <p className= "text-sm text-gray-300 mt-2">{spec.label}</p>
+                    </div>
+                )
+            )}
+            </div>
+
+
+
         </section>
 
     )
